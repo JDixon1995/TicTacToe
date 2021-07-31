b854149a1b7aa82532e930b8cf74c2069d8b5bff
John D <jddhalogamer@gmail.com>
2021-07-31 17:53:11 -0500

    built logic for marking board, and switching turns.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //TicTacToe
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+const cellElements = document.querySelectorAll('[data-cell]');
+
+let oTurn;
+
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, {once: true})
+});
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = oTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+
+    //Check for Win
+
+    //Check for Draw
+
+    //Switch Turns
+    swapTurn();
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurn() {
+    oTurn = !oTurn;
+}
